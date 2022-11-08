6838fc602b6d15fb32e9359103d4aa784d219999
xvburak <xvburak@vutbr.cz>
2022-11-08 14:15:41 +0100

    Update AboutSection.svelte

--- src/lib/components/sections/AboutSection.svelte
+++ src/lib/components/sections/AboutSection.svelte
@@ -1,6 +1,6 @@
 <div class=" bg-purple min-h-screen w-full flex justify-center items-center">
     
-
+    omg
 </div>
 
 <div class=" bg-purple min-h-screen w-full flex justify-center items-center">
