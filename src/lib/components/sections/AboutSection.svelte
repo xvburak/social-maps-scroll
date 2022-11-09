132409b6f600cc5d47351dea298699d28ab8d050
xvburak <xvburak@vutbr.cz>
2022-11-09 13:46:20 +0100

    size

--- src/lib/components/sections/AboutSection.svelte
+++ src/lib/components/sections/AboutSection.svelte
@@ -1,9 +1,43 @@
-<div class=" bg-purple min-h-screen w-full flex justify-center items-center">
-    
-    omg
+<div class=" bg-purple min-h-screen w-full flex space-x-4 p-4">
+    <div class="w-full md:w-1/2 pt-20">
+        <p>SocialMaps je aplikace, která nabízí kolektivně vytvářené mapy a (re)prezentuje místa, jež oproti jiným
+            mapovým službám nejsou hierarchizována, neprochází schvalovacím procesem nadřazené a neznámé autority,
+            nejsou nijak algoritmizována, nepodléhají placené reklamě a o veškerých procesech rozhoduje pouze
+            uživatelská komunita. </p>
+        <p>Snahou je tak proces mapování přeměnit v sociální aktivitu, která nekončí přímočarým sledováním zvýrazněné
+            trajektorie, cestu neurčuje nejsnadnější a nejrychlejší překonání trasy od startu k cíli. SocialMaps nabízí
+            obsah,k němuž se dá vztáhnout jak geograficky,tak i historicky a společensky.</p>
+        <p>SocialMaps jsou nástrojem, jehož cílemje formovat komunitu, která svět jenom nekonzumuje, ale taky prožívá a
+            pokouší se jej mapovat jinak, než počtem hvězdiček nebo mírou kyselosti kávy. ✨</p>
+    </div>
+    <div class="w-1/2">
+        <img src="/mockup.webp" alt="mockup aplikace">
+    </div>
 </div>
 
-<div class=" bg-purple min-h-screen w-full flex justify-center items-center">
-    
-
+<div class=" bg-purple min-h-screen w-full flex space-x-4 p-4">
+    <div class="w-full md:w-1/2 pt-20">
+        <p>Mapy totiž nejsou nikdy nositeli objektivní skutečnosti a nikdy nezaznamenávají mapované území neutrálním
+            způsobem. Záleží vždy na tom, kdo a za jakým účelem je vytváří.</p>
+        <p>A podle toho taky ovlivňují to, jakým způsobem o mapách smýšlíme, jak je používáme, jaká je jejich podstata,
+            komu a jak mají a mohou sloužit a jaký svět nám pomáhají reprezentovat, ale i poznávat; co nám mohou říci o
+            světě a schopnosti zapojení člověka v procesu tvorby map.</p>
+        <p>Principy a mechanismy, na kterých současné mapové služby fungují, vytváří značně limitující bariéru pro to,
+            co nám mohou mapy sdělovat, jak se díky nim městem pohybujeme a jak své okolí vnímáme. Proto se je
+            SocialMaps snaží zcela eliminovat.</p>
+    </div>
+    <div class="w-1/2">
+        <img src="/groups.webp" alt="ilustrační obrázek propojení">
+    </div>
 </div>
+
+
+<style>
+    div {
+        @apply text-white
+    }
+
+    p {
+        @apply mb-4 last-of-type:mb-0;
+    }
+</style>
